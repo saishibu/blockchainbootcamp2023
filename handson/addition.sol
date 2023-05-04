46b5be37122b7311966fc8003169d267fe6df134
Sai Shibu N B <saishibu38380@gmail.com>
2023-05-04 10:04:15 +0530

    Create addition.sol

---
+++ handson/addition.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.2 <0.9.0;
+contract sum{
+   uint public a;
+   uint public b;
+   uint public c;
+
+function addition(uint _a, uint _b) public {
+       a = _np;
+       b = _nc;
+       c = a + b;
+   }
